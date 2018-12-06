07c41e45b3bf2b71057a9761494ee3cbae85bfb0
pxvu97 <pxvu97@gmail.com>
2018-12-06 13:52:11 +0700

    edit swagger and converter

--- src/main/java/com/dxc/doctor/util/ProfileUtil.java
+++ src/main/java/com/dxc/doctor/util/ProfileUtil.java
@@ -1,11 +1,10 @@
 package com.dxc.doctor.util;
 
 import com.dxc.doctor.api.model.MedicalTreatmentProfile;
+import com.dxc.doctor.entity.DiseasesHistory;
 import com.dxc.doctor.entity.MedicalTreatmentProfileEntity;
 import org.joda.time.LocalDate;
 
-import java.math.BigDecimal;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class ProfileUtil {
@@ -15,8 +14,7 @@ public class ProfileUtil {
         profile.setCreatedDate(LocalDate.fromDateFields(entity.getCreateDate()));
         profile.setModifiedDate(LocalDate.fromDateFields(entity.getModifiedDate()));
         profile.setProfileId(entity.getProfileId());
-        profile.setDiseasesHistory(entity.getDiseasesHistory().stream().map(Objects ->
-        { return Objects.getName();}).collect(Collectors.toList()));
+        profile.setDiseasesHistory(entity.getDiseasesHistory().stream().map(DiseasesHistory::getName).collect(Collectors.toList()));
         profile.setMedicalTestResult(TestResultUtil.entity2TestResult(entity.getMedicalTestResult()));
         profile.setDoctor(entity.getDoctor());
         profile.setDoctorUpdated(entity.getDoctorUpdated());
