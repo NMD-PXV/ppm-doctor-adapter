f9043cd2a5ae94ba16844156d4474cc5fb553c54
pxvu97 <pxvu97@gmail.com>
2018-12-10 15:22:06 +0700

    search by ids, disease, name and edit swagger

--- src/main/java/com/dxc/doctor/repository/PrescriptionRepository.java
+++ src/main/java/com/dxc/doctor/repository/PrescriptionRepository.java
@@ -8,8 +8,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface PrescriptionRepository extends JpaRepository<PrescriptionEntity, Long> {
     PrescriptionEntity findByIdEquals(Long prescriptionId);
+
+    @Query("select p.medicalTreatmentProfileEntity.id from PrescriptionEntity p where p.id in :ids")
+    Set<Long> findProfileIds(@Param("ids") Set<Long> ids);
 }
